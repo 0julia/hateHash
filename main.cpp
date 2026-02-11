1fc81cdd10b29493efc2123fae4f375b7a429a83
julia <455929@bsd48.org>
2026-02-11 06:54:43 +0000

    restart

---
+++ main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <cstring>    // For strcmp()
+#include <cmath>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+//Student info
+struct student{
+  char firstname[40];
+  char lastname[40];
+  int studentid;
+  float gpa;
+};
+
+//defining functions
+void add(vector<student*>& bigtest);
+void print(vector<student*>& bigtest);
+void remove(vector<student*>& bigtest);
+
+int main(){
+  char command[20] = "";
+
+  //define vector
+  vector<student*> bigstruct;
+
+  //while the user hasn't quit the loop...
+  while(strcmp(command, "QUIT") != 0)
+    {
+      //figure out what they want to do
+      cout<< endl << "What would you like to do?" << endl;
+      cin >> command;
+
+    if(strcmp(command, "ADD") == 0){
+      add(bigstruct);
+      
+    }else if(strcmp(command, "PRINT") == 0){
+      print(bigstruct);
+
+    }else if(strcmp(command, "DELETE") == 0){
+      remove(bigstruct);
+ 
+    }else if(strcmp(command, "QUIT") != 0) {
+	cout << "That is not a valid command" << endl << "The valid commands are: ADD, DELETE, PRINT, or QUIT" << endl;
+    }
+    }
+  cout << "Bye bye!!!";
+
+  for (student* s : bigstruct){
+    delete s;
+  }
+  bigstruct.clear();
+  
+  return 0;
+}
+
+
+
+
+
+void add(vector<student*>& bigstruct)
+{
+  cout << "You just added a student to the repository" << endl;
+  
+      student* littlestruct = new student();
+
+      cout << "What is the student's first name?" << endl;
+      cin >> littlestruct->firstname;
+
+      cout << "What is the student's last name?" << endl;
+      cin >> littlestruct->lastname;
+
+      cout << "What is the student's ID?" << endl;
+      cin >> littlestruct->studentid;
+
+      cout << "What is the student's GPA?" << endl;
+      cin >> littlestruct->gpa;
+
+      bigstruct.push_back(littlestruct);
+
+      //  return bigtest;
+}
+
+void print(vector<student*>& bigstruct)
+{
+  for (size_t i = 0; i < bigstruct.size(); ++i) {
+    cout << "Name: " << bigstruct[i]->firstname << " " << bigstruct[i]->lastname << ", Student ID: " << bigstruct[i]->studentid << ", Student's GPA: " << fixed << setprecision(2) << bigstruct[i]->gpa << endl;
+  }
+}
+
+void remove(vector<student*>& bigstruct)
+{
+        int id;
+      cout << "What is the ID of the student you would like to delete?" << endl;
+      cin >> id;
+      auto it = bigstruct.begin();
+    while (it != bigstruct.end()) {
+      if ((*it)->studentid == id) {
+	delete *it;
+            it = bigstruct.erase(it); // Erase the element and get a new iterator
+            break; // Assuming only one element needs to be removed
+        } else {
+            ++it;
+        }
+    }
+}
